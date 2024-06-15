e9a5c0e22fcc7bdac65665f92f941c8c5397a276
Tjark Gerken <96004366+TjarkGerken@users.noreply.github.com>
2024-06-16 00:16:14 +0200

    pyyyyrotechniik

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -2,10 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import IntegerType, StringType, StructType, TimestampType
 
-dbUrl = 'jdbc:mysql://my-app-mariadb-service:3306/popular'
-dbOptions = {"user": "root", "password": "mysecretpw"}
-dbSchema = 'popular'
-
 windowDuration = '1 minute'
 slidingDuration = '1 minute'
 
@@ -24,7 +20,7 @@ kafkaMessages = spark \
     .format("kafka") \
     .option("kafka.bootstrap.servers",
             "my-cluster-kafka-bootstrap:9092") \
-    .option("subscribe", "tracking-data") \
+    .option("subscribe", "spotify-track-data") \
     .option("startingOffsets", "earliest") \
     .load()
 
@@ -53,18 +49,7 @@ trackingMessages = kafkaMessages.select(
     .withColumnRenamed('json.mission', 'mission') \
     .withWatermark("parsed_timestamp", windowDuration)
 
-# Example Part 4
-# Compute most popular slides
-popular = trackingMessages.groupBy(
-    window(
-        column("parsed_timestamp"),
-        windowDuration,
-        slidingDuration
-    ),
-    column("mission")
-).count() \
- .withColumnRenamed('window.start', 'window_start') \
- .withColumnRenamed('window.end', 'window_end') \
+
 
 # Example Part 5
 # Start running the query; print running counts to the console
@@ -74,22 +59,9 @@ consoleDump = popular \
     .format("console") \
     .start()
 
-# Example Part 6
 
 
-def saveToDatabase(batchDataframe, batchId):
-    global dbUrl, dbSchema, dbOptions
-    print(f"Writing batchID {batchId} to database @ {dbUrl}")
-    batchDataframe.distinct().write.jdbc(dbUrl, dbSchema, "overwrite", dbOptions)
 
-
-# Example Part 7
-dbInsertStream = popular \
-    .select(column('mission'), column('count')) \
-    .writeStream \
-    .outputMode("complete") \
-    .foreachBatch(saveToDatabase) \
-    .start()
 
 # Wait for termination
 spark.streams.awaitAnyTermination()
