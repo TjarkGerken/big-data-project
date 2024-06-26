5d51ae895cbf8b241056576a1982bb0ba828d5c7
Tjark Gerken <96004366+TjarkGerken@users.noreply.github.com>
2024-06-26 09:17:25 +0200

    checkpoint

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -47,12 +47,16 @@ parsedMessages = parsedMessages.withColumn("endTime", col("endTime").cast("times
 windowSpec = Window.partitionBy("UID").orderBy(unix_timestamp(col("endTime")))
 
 # Calculate total play time for each track and artist in each window
-totalPlayTime = parsedMessages\
-    .groupBy("UID", F.window("endTime", "7 days"), "trackName", "artistName")\
+totalPlayTime = parsedMessages \
+    .groupBy("UID", F.window("endTime", "7 days").alias("window"), "trackName", "artistName") \
     .agg(sum("msPlayed").alias("totalPlayTime"))
 
+
+windowSpecRank = Window.partitionBy("UID", "window.start").orderBy(col("totalPlayTime").desc())
+
 # Rank tracks and artists by total play time within each window
-rankedTracksAndArtists = totalPlayTime.withColumn("rank", rank().over(windowSpec))
+rankedTracksAndArtists = totalPlayTime.withColumn("rank", rank().over(windowSpecRank))
+
 
 # Keep only the top 10 tracks and artists in each window
 top10TracksAndArtists = rankedTracksAndArtists.filter(col("rank") <= 10)
